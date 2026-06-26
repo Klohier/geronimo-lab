75ed4aceedfaa907860d33b38d0790c8165ad84a
Keiji Lohier <keiji.lohier@gmail.com>
2026-06-26 11:26:33 -0400

    Move ansible user to blueprint

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,15 @@
 
 locals {
-  image_id = "local:import/bootc-fedora-44-qcow2-x86_64.qcow2"
+  image_id = "local:import/geronimo-base.qcow2"
 
   all_vms = merge(
-    { for i, vm in module.vms : "vm-${i + 1}" => vm },
     { "caddy-01" = module.caddy },
-    { "nginx-01" = module.nginx }
+    { "nginx-01" = module.nginx },
+    { "runner-01" = module.runner }
   )
 }
 
-module "vms" {
-  source         = "./modules/vm/"
-  count          = 2
-  vm_name        = "vm-${count.index + 1}"
-  image_id       = local.image_id
-  venv_node_name = var.venv_node_name
-  datastore_id   = var.datastore_id
-  tags           = ["bootc"]
-  ip_octet       = 60 + count.index
-}
+
 
 module "caddy" {
   source         = "./modules/vm/"
@@ -46,5 +37,19 @@ module "nginx" {
   disk_size      = 20
   tags           = ["bootc"]
   ip_octet       = 207
+}
+
+module "runner" {
+  source         = "./modules/vm/"
+  vm_name        = "runner-01"
+  vm_id          = 200
+  image_id       = local.image_id
+  venv_node_name = var.venv_node_name
+  datastore_id   = var.datastore_id
+  cores          = 2
+  memory         = 2048
+  disk_size      = 20
+  tags           = ["bootc", "runner"]
+  ip_octet       = 210
 }
 
