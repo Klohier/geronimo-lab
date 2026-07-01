5ebe2e2470822bc350f8878541963d0513a9b7c5
Keiji Lohier <keiji.lohier@gmail.com>
2026-07-01 14:11:45 -0400

    Container file is now multi stage with added k3s, as well in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,7 @@
 
 locals {
-  image_id = "local:import/geronimo-base.qcow2"
+  image_id = "local:import/geronimo-base2.qcow2"
+  image_k3 = "local:import/geronimo-k3s.qcow2"
 
   all_vms = merge(
     { "caddy-01" = module.caddy },
@@ -53,3 +54,18 @@ module "runner" {
   ip_octet       = 210
 }
 
+
+module "k3s-server" {
+  source         = "./modules/vm/"
+  vm_name        = "k3s-server"
+  vm_id          = 300
+  image_id       = local.image_k3
+  venv_node_name = var.venv_node_name
+  datastore_id   = var.datastore_id
+  cores          = 2
+  memory         = 2048
+  disk_size      = 20
+  tags           = ["bootc", "k3s-server"]
+  ip_octet       = 220
+
+}
